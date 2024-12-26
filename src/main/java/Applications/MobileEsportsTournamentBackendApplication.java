ca11ec59bf2e850c308b29f5e76b4d5ee5cf35f8
Kyle Petrick <kppetrick@gmail.com>
2024-12-26 15:24:30 -0600

    Name changes

--- src/main/java/Applications/MobileEsportsTournamentBackendApplication.java
+++ src/main/java/Applications/MobileEsportsTournamentBackendApplication.java
@@ -1,4 +1,4 @@
-package Applications.java;
+package Applications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/Applications/MobileEsportsTournamentBackendApplicationTests.java
+++ src/test/java/Applications/MobileEsportsTournamentBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package Applications.java;
+package Applications;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
