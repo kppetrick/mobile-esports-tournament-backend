fe3df9c34900e8158079c8c99f189b2c58800ae0
Kyle Petrick <kppetrick@gmail.com>
2024-12-26 15:07:52 -0600

    Initial commit

---
+++ src/main/java/com/example/mobile_esports_tournament_backend/MobileEsportsTournamentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobile_esports_tournament_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileEsportsTournamentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileEsportsTournamentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobile_esports_tournament_backend/MobileEsportsTournamentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobile_esports_tournament_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileEsportsTournamentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
