5e1bc53de52ca25477ddfd9e0b671ae55f13cf44
Kyle Petrick <kppetrick@gmail.com>
2024-12-26 21:03:15 -0600

    Initial project setup with PostgreSQL configuration

---
+++ src/main/java/Applications/controllers/TestController.java
@@ -0,0 +1,14 @@
+package Applications.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Database connection is successful!";
+    }
+}
+
